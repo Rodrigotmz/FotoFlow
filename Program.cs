71dcfb5ae7ae9cc591017ebc591debae0fa2a8b3
RodrigoMtz <rodrigomartinezdev@outlook.com>
2026-04-13 18:19:27 -0600

    feat(startup): reopen last used mode

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ namespace FotoFlow
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmFotoFlow());
+
+            var settings = FotoFlow.Core.AppUserSettings.Load();
+            var startMode = settings.LastMode ?? FotoFlow.Core.FotoFlowMode.Basic;
+            FotoFlow.Core.AppRuntimeState.LastMode = startMode;
+
+            Application.ApplicationExit += (_, _) =>
+            {
+                FotoFlow.Core.AppUserSettings.Update(s => s.LastMode = FotoFlow.Core.AppRuntimeState.LastMode);
+            };
+
+            Form startForm = startMode == FotoFlow.Core.FotoFlowMode.Advance
+                ? new FrmFotoFlowAdvance()
+                : new FrmFotoFlow();
+
+            Application.Run(startForm);
         }
     }
 }
